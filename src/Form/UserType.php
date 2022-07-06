c06649374f5de5570aa00a0d727c2c75955347b5
Tarek Oussabi <oussabi.tarek@gmail.com>
2022-07-07 00:08:34 +0100

    adding hash password

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -8,11 +8,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-<<<<<<< HEAD
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
-=======
->>>>>>> 710b1e17ffde3d1766341e3a94829f5b87e13d13
 
 class UserType extends AbstractType
 {
@@ -33,16 +30,12 @@ class UserType extends AbstractType
             ->add('prenom')
             ->add('adresse')
             ->add('telephone')
-<<<<<<< HEAD
             ->add('password',RepeatedType::class,[
                 "type"=>PasswordType::class,
                 "first_options"=>["label"=>"Mot de passe "],
                 "second_options"=>["label"=>"Confirmation"]
-                ])
-        ;
-=======
-            ->add('password', PasswordType::class);
->>>>>>> 710b1e17ffde3d1766341e3a94829f5b87e13d13
+            ]);
+        
     }
 
     public function configureOptions(OptionsResolver $resolver): void
