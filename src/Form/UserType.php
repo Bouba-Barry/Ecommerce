9eb427d62f8f38aac995da782ff814d607f7a36e
barry <bboubacar366@gmail.com>
2022-07-07 18:21:50 +0100

    changer les roles en les commançant par roles_NomRoles

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -19,15 +19,9 @@ class UserType extends AbstractType
             ->add('email')
             ->add('roles', ChoiceType::class, [
                 'choices' => [
-<<<<<<< HEAD
-                    'SUPER_AdMIN' => 'ROLE_SUPER_ADMIN',
-                    'ADMIN' => 'ROLE_ADMIN',
-                    'COMPTABLE' => 'ROLE_COMPTABLE',
-=======
                     'ROLE_SUPER_AdMIN' => 'ROLE_SUPER_ADMIN',
                     'ROLE_ADMIN' => 'ROLE_ADMIN',
-                    'ROLE_COMPTABLE' => 'ROLE_COMPTABLE',
->>>>>>> 799e149 (securiser l access au controller)
+                    'ROLE_COMPTABLE' => 'ROLE_COMPTABLE'
                 ],
                 'multiple' => true,
                 'expanded' => true
