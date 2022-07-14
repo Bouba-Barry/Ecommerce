c988268299596f8c1f39647cb92b03d237793b5e
Tarek Oussabi <oussabi.tarek@gmail.com>
2022-07-14 15:00:44 +0100

    adding panier

---
+++ public/js/panier.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let addcard = document.getElementsByClassName("addcard");
+  let buynow = document.getElementById("buy");
+
+  buynow.addEventListener("click", function () {
+    console.log(1);
+  });
+  console.log(addcard);
+  var vals = [];
+  for (let i = 0; i < addcard.length; i++) {
+    addcard[i].addEventListener("click", test);
+    function test() {
+      testclick();
+    }
+
+    function testclick() {
+      var matches = addcard[i].id.match(/(\d+)/);
+      //   sessionStorage.setItem("clone", matches[0]);
+      vals.push(matches[0]);
+      addcard[i].innerHTML = "added to card";
+      addcard[i].style.backgroundColor = "rgb(246, 139, 30)";
+      addcard[i].style.border = "none";
+      addcard[i].style.outline = "none";
+    }
+  }
+
+  var noti = document.getElementById("panier");
+
+  for (var but of addcard) {
+    but.addEventListener("click", (e) => {
+      var add = Number(noti.getAttribute("data-count") || 0);
+      if (add !== 0) {
+        noti.setAttribute("data-count", add + 1);
+        noti.classList.add("zero");
+      } else {
+        noti.setAttribute("data-count", add + 1);
+        noti.classList.add("zero");
+      }
+    });
+  }
+});
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/admin')]
+// #[Route('/admin')]
 class HomeController extends AbstractController
 {
-    #[Route('', name: 'app_home')]
+    #[Route('/admin', name: 'app_home_admin')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
     }
+    #[Route('', name: 'app_home')]
+    public function home(ProduitRepository $produitRepository): Response
+    {
+        return $this->render('home.html.twig', [
+            'produits' => $produitRepository->findAll(),
+        ]);
+    }
+
 }
