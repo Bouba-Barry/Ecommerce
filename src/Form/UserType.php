37294fc5307b89b7044272756e9a07cbf4531f71
Tarek Oussabi <oussabi.tarek@gmail.com>
2022-07-16 22:35:40 +0100

    adding form registration for new user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,17 +6,19 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email',EmailType::class, array('label'   => false,'attr'=>['placeholder' => 'Email','class'=>'form-control pl-15 bg-transparent text-white plc-white']))
             ->add('roles', ChoiceType::class, [
                 'choices' => [
                     'ROLE_SUPER_AdMIN' => 'ROLE_SUPER_ADMIN',
@@ -24,16 +26,18 @@ class UserType extends AbstractType
                     'ROLE_COMPTABLE' => 'ROLE_COMPTABLE'
                 ],
                 'multiple' => true,
-                'expanded' => true
-            ])
-            ->add('nom')
-            ->add('prenom')
-            ->add('adresse')
-            ->add('telephone')
+                'expanded' => true,
+                'label'   => false
+            ],array('attr'=>['class'=>'form-control pl-15 bg-transparent text-white plc-white']))
+            ->add('nom',TextType::class,array('label'   => false,'attr'=>['placeholder' => 'Nom','class'=>'form-control pl-15 bg-transparent text-white plc-white']))
+            ->add('prenom',TextType::class,array('label'   => false,'attr'=>['placeholder' => 'Prenom','class'=>'form-control pl-15 bg-transparent text-white plc-white']))
+            ->add('adresse',TextType::class,array('label'   => false,'attr'=>['placeholder' => 'Adresse','class'=>'form-control pl-15 bg-transparent text-white plc-white']))
+            ->add('telephone',TelType::class,array('label'   => false,'attr'=>['placeholder' => 'Telephone','class'=>'form-control pl-15 bg-transparent text-white plc-white']))
             ->add('password', RepeatedType::class, [
                 "type" => PasswordType::class,
-                "first_options" => ["label" => "Mot de passe "],
-                "second_options" => ["label" => "Confirmation"]
+                "first_options" => ["label" => false,'attr'=>['placeholder' => 'Mot de passe','class'=>'form-control pl-15 bg-transparent text-white plc-white']],
+                "second_options" => ["label" => false,'attr'=>['placeholder' => 'Confirmation','class'=>'form-control pl-15 bg-transparent text-white plc-white']],
+                
             ]);
     }
 
