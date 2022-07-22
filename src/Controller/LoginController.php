2546b8546b8d2accaa70b95f88cc4df7e9e1c500
barry <bboubacar366@gmail.com>
2022-07-22 01:01:39 +0100

    add login form

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,48 +6,61 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 // #[Security("is_granted('ROLE_USER')")]
 class LoginController extends AbstractController
 {
   #[Route('/login', name: 'app_login')]
-  public function index(AuthenticationUtils $authenticationUtils): Response
+  #[Route('/user/login', name: 'app_login')]
+  public function index(Request $request, AuthenticationUtils $authenticationUtils): Response
   {
+
+    // dd($request->getPathInfo());
+
     // get the login error if there is one
     $error = $authenticationUtils->getLastAuthenticationError();
 
     // last username entered by the user
     $lastUsername = $authenticationUtils->getLastUsername();
-  //   $plaintext_password = "tarek";
+    //   $plaintext_password = "tarek";
 
-//   $crypted = password_hash('tarek', PASSWORD_DEFAULT);
-// dd($crypted);
-  // // The hashed password retrieved from database
-  // $hash = 
-  //   '$2y$10$He5nuFPBPHbVYaaXavsVu.zbsIyILENNAiOz6wB.fVl0qWh43Rb3O';
-  
-  // // Verify the hash against the password entered
-  // $verify = password_verify($plaintext_password, $hash);
-  
-  // // Print the result depending if they match
-  // if ($verify) {
-  //     dd('Password Verified!');
-  // } else {
-  //     dd('Incorrect Password!');
-  // } 
+    //   $crypted = password_hash('tarek', PASSWORD_DEFAULT);
+    // dd($crypted);
+    // // The hashed password retrieved from database
+    // $hash = 
+    //   '$2y$10$He5nuFPBPHbVYaaXavsVu.zbsIyILENNAiOz6wB.fVl0qWh43Rb3O';
+
+    // // Verify the hash against the password entered
+    // $verify = password_verify($plaintext_password, $hash);
+
+    // // Print the result depending if they match
+    // if ($verify) {
+    //     dd('Password Verified!');
+    // } else {
+    //     dd('Incorrect Password!');
+    // } 
 
 
 
-       if ($error ) {
-              dd($error);
-          }
-    return $this->render('login/index.html.twig', [
-      'last_username' => $lastUsername,
-      'error'         => $error,  
-    ]);
+    if ($error) {
+      dd($error);
+    }
+
+    if ($request->getPathInfo() === "/login") {
+      return $this->render('login/index.html.twig', [
+        'last_username' => $lastUsername,
+        'error'         => $error,
+      ]);
+    } else {
+      return $this->render('frontend/login.html.twig', [
+        'last_username' => $lastUsername,
+        'error'         => $error,
+      ]);
+    }
   }
- 
+
 
   /**
    * @Route("/admin/logout", name="app_logout", methods={"GET"})
