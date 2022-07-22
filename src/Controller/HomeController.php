c8ade0b7fb1b268bbc04bb05022985e95b5888a7
Tarek Oussabi <oussabi.tarek@gmail.com>
2022-07-22 12:10:00 +0100

    panier work

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,25 +2,65 @@
 
 namespace App\Controller;
 
+use doctrine;
+use App\Entity\User;
 use App\Entity\Panier;
 use App\Entity\Produit;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Mapping\Id;
 use App\Entity\SousCategorie;
-use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use App\Repository\PanierRepository;
 use App\Repository\ProduitRepository;
 use App\Repository\SousCategorieRepository;
-use App\Repository\UserRepository;
-use Doctrine\ORM\Mapping\Id;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Length;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Validator\Constraints\Length;
 
 // #[Route('/admin')]
 class HomeController extends AbstractController
 {
 
+    #[Route('/panier_infos/{id}', name: 'app_panier_infos',methods: ['GET'])]
+    public function panier_infos(User $user,PanierRepository $panierRepository,ManagerRegistry $doctrine){
+
+        
+        $entityManager = $doctrine->getManager();
+        $panier=new Panier();
+        
+        $panier->setUser($user);
+        $entityManager->persist($panier);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+        
+     
+        dd("fh");
+        return $this->render('home.html.twig'
+
+        );         
+  
+    }
+
+
+
+
+
+    #[Route('/exp', name: 'app_exp',methods: ['GET'])]
+    public function exp(ProduitRepository $produitRepository){
+
+
+        return $this->render('home.html.twig',[
+            'produits' => $produitRepository->findAll(),
+        ]
+
+        );         
+  
+    }
 
     #[Route('/shop_details/{id}', name: 'app_shp_details',methods: ['GET'])]
      public function shop_details($id,ProduitRepository $produitRepository,SousCategorieRepository $sousCategorieRepository){
@@ -120,35 +160,56 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/panier/{id}', name: 'app_panier', methods: ['GET'])]
-    public function panier($id, ProduitRepository $produitRepository, PanierRepository $panierRepository): Response
+    #[Route('/panier/{id}/{slug}/{user_id}', name: 'app_panier', methods: ['GET'])]
+    public function panier($id,$slug,$user_id,UserRepository $userRepository,ManagerRegistry $doctrine ,ProduitRepository $produitRepository, PanierRepository $panierRepository): Response
     {
+
+        $entityManager = $doctrine->getManager();
+        $panier=new Panier();
+        $user=$userRepository->find($user_id);
+        $panier->setUser($user);
+        $entityManager->persist($panier);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+        
 
         // $val->array_push($produit->getId());
         $d = str_split($id);
-        $array_ids = [];
+        $f=str_split($slug);
+         $panier_id=$panier->getId();
+        // $array_ids = [];
         for ($i = 0; $i < count($d); $i++) {
-            if ($d[$i] !== ",")
-                array_push($array_ids, $d[$i]);
-        }
-        $panier = new Panier();
+            if ($d[$i] !== "," && $f[$i]!==","){
+                // $produit=$produitRepository->find($d[$i]);
+                // $panier->addProduit($produit);
 
-        if ($this->getUser()) {
 
-            $panier->SetUser($this->getUser());
-            $panier->SetMontant(4);
-            // $produit = $produitRepository->find($id)
-            foreach ($array_ids as $id) {
+                $panierRepository->add_to_produit_panier($panier_id,$d[$i],$f[$i]);
+               
+              
 
-                $produit = $produitRepository->find($id);
-                $panier->addProduit($produit);
+
             }
-            $panierRepository->add($panier, true);
         }
+        dd("fin am3lm");       
+        
+        // if ($this->getUser()) {
+
+        //     $panier->SetUser($this->getUser());
+        //     $panier->SetMontant(4);
+        //     // $produit = $produitRepository->find($id)
+        //     foreach ($array_ids as $id) {
+
+        //         $produit = $produitRepository->find($id);
+        //         $panier->addProduit($produit);
+        //     }
+        //     $panierRepository->add($panier, true);
+        // }
 
 
-        return $this->render('panier.html.twig', [
-            'produits' => $produitRepository->findBy(['id' => $array_ids]),
-        ]);
+        // return $this->render('panier.html.twig', [
+        //     'produits' => $produitRepository->findBy(['id' => $array_ids]),
+        // ]);
     }
 }
