57f8a832e11b2009686727ba88fb614d5036c7b9
barry <bboubacar366@gmail.com>
2022-07-22 03:09:11 +0100

    registration and login User

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,21 +22,22 @@ class HomeController extends AbstractController
 {
 
 
-    #[Route('/shop_details/{id}', name: 'app_shp_details',methods: ['GET'])]
-     public function shop_details($id,ProduitRepository $produitRepository,SousCategorieRepository $sousCategorieRepository){
+    #[Route('/shop_details/{id}', name: 'app_shp_details', methods: ['GET'])]
+    public function shop_details($id, ProduitRepository $produitRepository, SousCategorieRepository $sousCategorieRepository)
+    {
         // dd($produit);
 
-        $produit=$produitRepository->find($id);
+        $produit = $produitRepository->find($id);
 
 
-        $produits_similaires=$produitRepository->findBy(['sous_categorie' => $produit->getSousCategorie()]);
-      
+        $produits_similaires = $produitRepository->findBy(['sous_categorie' => $produit->getSousCategorie()]);
 
-        $sous_categories=$sousCategorieRepository->findBy(['categorie' => $produit->getSousCategorie()->getCategorie() ]);
 
-      
+        $sous_categories = $sousCategorieRepository->findBy(['categorie' => $produit->getSousCategorie()->getCategorie()]);
 
-        $produits_en_relation=$produitRepository->findBy(['sous_categorie' => $sous_categories]);
+
+
+        $produits_en_relation = $produitRepository->findBy(['sous_categorie' => $sous_categories]);
 
         // dd($produits_en_relation);
         // dd($produits_similaires);
@@ -45,10 +46,8 @@ class HomeController extends AbstractController
             'produit' => $produit,
             'produits_similaires' => $produits_similaires,
             'produits_en_relation' => $produits_en_relation
-        ]);         
-  
-
-     }
+        ]);
+    }
 
 
 
@@ -123,7 +122,7 @@ class HomeController extends AbstractController
         $bestSellers = $produitRepository->BestSellers();
         // dd($bestSellers);
         $plusVendus = $produitRepository->MostBuy();
-        // dd($plusVendus);
+        dd($plusVendus);
         return $this->render('frontend/home.html.twig', [
             'produits' => $produitRepository->findAll(),
             'mostSaleMonth' => $MostSalesMonth,
--- src/Repository/ProduitRepository.php
+++ src/Repository/ProduitRepository.php
@@ -39,28 +39,28 @@ class ProduitRepository extends ServiceEntityRepository
         }
     }
 
-    /**
-     * @return Produit[] Returns an array of Produit objects
-     */
-    public function findRecentProduct(): array
-    {
-        // SELECT p FROM DoctrineExtensions\Query\BlogPost p WHERE DATEDIFF(CURRENT_TIME(), p.created) < 7 
-        return $this->createQueryBuilder('p')
-            //    ->andWhere('p.createAt = :val')
-            ->andWhere('DATEDIFF(CURRENT_TIMESTAMP(), p.createdAt)  BETWEEN 1 and 31 ')
-            // ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult();
-    }
-    public function findRecent()
+    // /**
+    //  * @return Produit[] Returns an array of Produit objects
+    //  */
+    // public function findRecentProduct(): array
+    // {
+    //     // SELECT p FROM DoctrineExtensions\Query\BlogPost p WHERE DATEDIFF(CURRENT_TIME(), p.created) < 7 
+    //     return $this->createQueryBuilder('p')
+    //         //    ->andWhere('p.createAt = :val')
+    //         ->andWhere('DATEDIFF(CURRENT_TIMESTAMP(), p.createdAt)  BETWEEN 1 and 31 ')
+    //         // ->setParameter('val', $value)
+    //         ->orderBy('p.id', 'ASC')
+    //         ->setMaxResults(10)
+    //         ->getQuery()
+    //         ->getResult();
+    // }
+    public function findRecentProduct()
     {
         $conn = $this->getEntityManager()->getConnection();
 
         $sql = "
         SELECT * FROM produit p
-        WHERE DATEDIFF(CURRENT_TIMESTAMP(), p.create_at)  BETWEEN 1 and 31 
+        WHERE DATEDIFF(CURRENT_TIMESTAMP(), p.create_at)  BETWEEN 0 and 15
         ORDER BY p.id ASC
         ";
         $stmt = $conn->prepare($sql);
@@ -102,6 +102,53 @@ class ProduitRepository extends ServiceEntityRepository
         SELECT p.* FROM produit p, commande_produit f
         WHERE p.id = f.produit_id and (DATEDIFF(CURRENT_TIMESTAMP(), f.create_at)  BETWEEN 1 and 31) 
         ORDER BY p.id ASC
+        ";
+        $stmt = $conn->prepare($sql);
+        $resultSet = $stmt->executeQuery();
+
+        // returns an array of arrays (i.e. a raw data set)
+        return $resultSet->fetchAllAssociative();
+    }
+
+    /**
+     * @return Produit[] Returns an array of Produit objects
+     */
+    public function BestSellers()
+    {
+
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = "
+        SELECT p.* FROM produit p, commande_produit f
+        WHERE p.id = f.produit_id 
+        GROUP BY f.produit_id
+        HAVING COUNT(*) > 1
+        ORDER BY p.id DESC
+    
+        ";
+        $stmt = $conn->prepare($sql);
+        $resultSet = $stmt->executeQuery();
+
+        // returns an array of arrays (i.e. a raw data set)
+        return $resultSet->fetchAllAssociative();
+    }
+
+
+    /**
+     * @return Produit[] Returns an array of Produit objects
+     */
+    public function MostBuy()
+    {
+
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = "
+        SELECT p.* FROM produit p, commande_produit f
+        WHERE p.id = f.produit_id 
+        GROUP BY f.produit_id
+        ORDER BY COUNT(*) DESC
+        LIMIT 1
+    
         ";
         $stmt = $conn->prepare($sql);
         $resultSet = $stmt->executeQuery();
