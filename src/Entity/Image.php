ae72bc3125348742bcdd0c1d75beb58e785c5aed
barry <bboubacar366@gmail.com>
2022-07-27 20:55:55 +0100

    add table Image and relation to prod and variation

---
+++ src/Entity/Image.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $urlImage;
+
+    #[ORM\ManyToOne(targetEntity: Produit::class, inversedBy: 'images')]
+    private $produit;
+
+    #[ORM\ManyToOne(targetEntity: Variation::class, inversedBy: 'images')]
+    private $variation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getProduit(): ?Produit
+    {
+        return $this->produit;
+    }
+
+    public function setProduit(?Produit $produit): self
+    {
+        $this->produit = $produit;
+
+        return $this;
+    }
+
+    public function getVariation(): ?Variation
+    {
+        return $this->variation;
+    }
+
+    public function setVariation(?Variation $variation): self
+    {
+        $this->variation = $variation;
+
+        return $this;
+    }
+}
